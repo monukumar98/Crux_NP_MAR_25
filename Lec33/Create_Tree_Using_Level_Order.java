1e82f825f2e850038144d01aad5389d721e47ef9
monukumar98 <monu012as@gmail.com>
2025-08-23 17:01:24 +0530

    Binary Search Tree

--- Lec33/Create_Tree_Using_Level_Order.java
+++ Lec33/Create_Tree_Using_Level_Order.java
@@ -28,6 +28,45 @@ public class Create_Tree_Using_Level_Order {
 	private void createTree() {
 		// TODO Auto-generated method stub
 		Queue<Node> q = new LinkedList<>();
+		int item=sc.nextInt();
+		Node nn = new Node(item);
+		root=nn;
+		q.add(nn);
+		while(!q.isEmpty()) {
+			Node r=q.poll();
+			int c1=sc.nextInt();
+			int c2=sc.nextInt();
+			if(c1!=-1) {
+				Node node = new Node(c1);
+				r.left=node;
+				q.add(node);
+			}
+			if(c2!=-1) {
+				Node node = new Node(c2);
+				r.right=node;
+				q.add(node);
+			}
+		}
 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
